43cac437c3099cb9a3cada7f431c8e1a8519a2d9
Figglewatts <figglewatts@hotmail.co.uk>
2015-05-12 20:06:52 +0100

    Added Vec3 type to represent 3-dimensional vectors

---
+++ MapParse/MapParse/Types/Vec3.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+
+namespace MapParse.Types
+{
+	public class Vec3
+	{
+		public float X { get; set; }
+		public float Y { get; set; }
+		public float Z { get; set; }
+
+		public Vec3() { }
+
+		public Vec3(float x, float y, float z)
+		{
+			X = x;
+			Y = y;
+			Z = z;
+		}
+
+		public float Dot(Vec3 v3)
+		{
+			return (X * v3.X) + (Y * v3.Y) + (Z * v3.Z);
+		}
+
+		public Vec3 Cross(Vec3 v3)
+		{
+			float x, y, z;
+			x = Y * v3.Z - v3.Y * Z;
+			y = X * v3.Z - v3.X * Z;
+			z = X * v3.Y - v3.X * Y;
+			return new Vec3(x, y, z);
+		}
+
+		public void Normalize()
+		{
+			float d = X * X + Y * Y + Z * Z;
+			
+			if (d == 1F || d == 0F)
+			{
+				return;
+			}
+
+			d = (float)Math.Sqrt(d);
+			X *= d;
+			Y *= d;
+			Z *= d;
+		}
+
+		public static Vec3 operator+ (Vec3 a, Vec3 b)
+		{
+			return new Vec3(a.X + b.X, a.Y + b.Y, a.Z + b.Z);
+		}
+
+		public static Vec3 operator- (Vec3 a, Vec3 b)
+		{
+			return new Vec3(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+		}
+	}
+}
