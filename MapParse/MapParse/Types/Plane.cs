da1f6a168ce28a4c71c4878c1588affbfd9522aa
Figglewatts <figglewatts@hotmail.co.uk>
2015-05-12 20:06:03 +0100

    Added Plane type for mathematical definitions of a plane

---
+++ MapParse/MapParse/Types/Plane.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MapParse.Types
+{
+	public class Plane
+	{
+		public Vec3 Normal;
+		public float Distance = 0;
+
+		public Plane()
+		{
+			this.Normal = new Vec3();
+			this.Distance = 0;
+		}
+
+		public Plane(Vec3 n, float d)
+		{
+			this.Normal = n;
+			this.Distance = d;
+		}
+
+		public Plane(Vec3 a, Vec3 b, Vec3 c)
+		{
+			this.Normal = (c - b).Cross(a - b);
+			this.Normal.Normalize();
+			this.Distance = -this.Normal.Dot(a);
+		}
+	}
+}
