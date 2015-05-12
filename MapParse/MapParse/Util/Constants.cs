f41d182031b64121da38fbd4bb9bdcf45e1cd313
Figglewatts <figglewatts@hotmail.co.uk>
2015-05-12 20:07:16 +0100

    Added a static Constants class with an epsilon constant for floating-point comparison

---
+++ MapParse/MapParse/Util/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapParse.Util
+{
+	public static class Constants
+	{
+		public static const double Epsilon = 1e-5;
+	}
+}
