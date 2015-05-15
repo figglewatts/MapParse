3eb03e7672176a89d9699fc25225c386e33d8a26
Sam Gibson <figglewatts@hotmail.co.uk>
2015-05-15 09:07:09 +0100

    Added body to Face.cs

--- MapParse/MapParse/Types/Face.cs
+++ MapParse/MapParse/Types/Face.cs
@@ -5,6 +5,21 @@ namespace MapParse.Types
 {
 	public struct Face
 	{
-	
+		public Plane P { get; set; }
+		public Plane[] TexAxis { get; set; }
+		public float[] TexScale { get; set; }
+		public string Texture { get; set; }
+		public float Rotation { get; set; }
+		public DynamicArray<Poly> Polys { get; set; }
+		
+		public Face() 
+		{
+			TexAxis = new Plane[2];
+			TexScale = new float[2];
+			Polys = new DynamicArray<Poly>();
+			Texture = "";
+			P = new Plane();
+			Rotation = 0F;
+		}
 	}
 }
