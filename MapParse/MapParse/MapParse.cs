207dbe1a99ca09cc27bfad73c729cc5433f3d610
Figglewatts <figglewatts@hotmail.co.uk>
2015-05-15 15:19:05 +0100

    Added base class

---
+++ MapParse/MapParse/MapParse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MapParse.Types;
+using MapParse.Util;
+
+namespace MapParse
+{
+	public static class MapParse
+	{
+		
+	}
+}
