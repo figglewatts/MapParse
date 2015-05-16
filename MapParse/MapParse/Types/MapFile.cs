64561199ad63990291e8432200f4049f61304865
Figglewatts <figglewatts@hotmail.co.uk>
2015-05-16 18:16:08 +0100

    Added class to represent an entire map file

---
+++ MapParse/MapParse/Types/MapFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapParse.Types
+{
+	public class MapFile
+	{
+		public List<Entity> Entities { get; set; }
+
+		public MapFile()
+		{
+			Entities = new List<Entity>();
+		}
+	}
+}
