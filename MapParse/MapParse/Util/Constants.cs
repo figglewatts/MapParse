c059c00bffce76e62cfced625173aec570edf342
Figglewatts <figglewatts@hotmail.co.uk>
2015-05-19 08:14:31 +0100

    Added some documentation

--- MapParse/MapParse/Util/Constants.cs
+++ MapParse/MapParse/Util/Constants.cs
@@ -6,8 +6,14 @@ using System.Threading.Tasks;
 
 namespace MapParse.Util
 {
+	/// <summary>
+	/// A class containing constant values. Used for easy access to said constants.
+	/// </summary>
 	public static class Constants
 	{
+		/// <summary>
+		/// A value used for comparing floats. E.G. to determine if a float was equal to a value you'd see if it was within plus or minus Epsilon of the value.
+		/// </summary>
 		public const double Epsilon = 1e-5;
 	}
 }
