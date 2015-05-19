62227b9fdcd3cffbab54a4a4fcd03f6748e7f7aa
Figglewatts <figglewatts@hotmail.co.uk>
2015-05-19 11:33:32 +0100

    Added Property class for entity properties

---
+++ MapParse/MapParse/Types/Property.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapParse.Types
+{
+	public struct Property
+	{
+		public string Key { get; set; }
+		public string Value { get; set; }
+
+		public Property(string k, string v)
+		{
+			Key = k;
+			Value = v;
+		}
+	}
+}
