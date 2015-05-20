757babd81dd9c89faa7c28704b49d86cf590e75a
Figglewatts <figglewatts@hotmail.co.uk>
2015-05-20 07:22:27 +0100

    Added UTF-8 character constants for the parse method to use instead of magic numbers

--- MapParse/MapParse/Util/Constants.cs
+++ MapParse/MapParse/Util/Constants.cs
@@ -15,5 +15,16 @@ namespace MapParse.Util
 		/// A value used for comparing floats. E.G. to determine if a float was equal to a value you'd see if it was within plus or minus Epsilon of the value.
 		/// </summary>
 		public const double Epsilon = 1e-5;
+
+		public const int NEWLINE = 0x0A;
+		public const int CARRAIGE_RETURN = 0x0D;
+		public const int QUOTATION_MARK = 0x22;
+		public const int LEFT_PARENTHESIS = 0x28;
+		public const int RIGHT_PARENTHESIS = 0x29;
+		public const int LEFT_BRACKET = 0x5B;
+		public const int RIGHT_BRACKET = 0x5D;
+		public const int LEFT_BRACE = 0x7B;
+		public const int RIGHT_BRACE = 0x7D;
+		public const int SPACE = 0x20;
 	}
 }
