7203c3c79d4d3e81b24c6e22dd982df6eba4728c
Figglewatts <figglewatts@hotmail.co.uk>
2015-05-20 07:22:57 +0100

    Added Test project for testing stuff out

---
+++ MapParse/MapParseTests/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MapParse;
+using MapParse.Types;
+using MapParse.Util;
+
+namespace MapParseTests
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MapParser.ParseMap(@"E:\Documents\Unity Stuff\LSD Revamped\LSD Revamped\Assets\Maps\dr_pi.map");
+
+			/*Vec3 intersection;
+
+			Plane a = new Plane(new Vec3(1, 0, 0), 1);
+			Plane b = new Plane(new Vec3(0, 1, 0), 1);
+			Plane c = new Plane(new Vec3(0, 0, 1), 1);
+
+			PlaneUtil.GetIntersection(a, b, c, out intersection);
+
+			Console.WriteLine(intersection.ToString());*/
+
+			Console.ReadLine();
+		}
+	}
+}
